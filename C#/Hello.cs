eef7dcd76f2318bc53f0c5ea10dab7d56d3dd9d8
SJK85 <stephenkollar85@gmail.com>
2016-05-03 22:06:49 -0400

    configuring git and VS
    setting up VS and git to work together

---
+++ C#/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first C# program");
+            string something = Console.ReadLine();
+            Console.WriteLine(something);
+            Console.ReadKey();
+        }
+    }
+}
